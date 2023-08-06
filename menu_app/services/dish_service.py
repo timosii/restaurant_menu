f16d1ec26fd5ade7de9d9646d8a6c0f3025237be
timosii <holdcarter@holdcarter.ru>
2023-08-06 20:55:33 +0300

    realize cache need work on cascade delete

--- menu_app/services/dish_service.py
+++ menu_app/services/dish_service.py
@@ -1,27 +1,62 @@
+import json
+import redis
+from fastapi.encoders import jsonable_encoder
 from fastapi import Depends
-from ..schemas import DishIn
+from ..schemas import DishIn, DishOut
 from ..repositories.dish_repository import DishRepository
 from uuid import UUID
+from typing import Optional
+
+
+class CacheDish:
+    def __init__(self, host='localhost', port=6379, db=0):
+        self.redis_conn = redis.Redis(host=host, port=port, db=db)
+
+    def save_cache(self, subject: DishOut, dish_id: UUID) -> None:
+        self.redis_conn.set(f'dish:{dish_id}', json.dumps(
+            jsonable_encoder(subject)))
+
+    def load_cache(self, dish_id: UUID) -> Optional[DishOut]:
+        cached_data = self.redis_conn.get(f'dish:{dish_id}')
+        if cached_data:
+            return DishOut(**json.loads(cached_data))
+        return None
+
+    def check_cache(self, dish_id: UUID) -> bool:
+        return self.redis_conn.exists(f'dish:{dish_id}')
+
+    def delete_cache(self, dish_id: UUID) -> int:
+        return self.redis_conn.delete(f'dish:{dish_id}')
 
 
 class DishService:
 
     def __init__(self, database_repository: DishRepository = Depends()):
         self.database_repository = database_repository
+        self.cache = CacheDish()
 
     def get_all(self, submenu_id: UUID):
         return self.database_repository.get_dishes(submenu_id=submenu_id)
 
     def get_one(self, dish_id: UUID):
-        return self.database_repository.get_dish(dish_id=dish_id)
+        if self.cache.check_cache(dish_id=dish_id):
+            return self.cache.load_cache(dish_id=dish_id)
+        result = self.database_repository.get_dish(dish_id=dish_id)
+        self.cache.save_cache(subject=result, dish_id=result.id)
+        return result
 
     def create(self, submenu_id: UUID, dish: DishIn):
-        return self.database_repository.create_dish(
+        result = self.database_repository.create_dish(
             submenu_id=submenu_id, dish=dish)
+        self.cache.save_cache(subject=result, dish_id=result.id)
+        return result
 
     def update(self, submenu_id: UUID, dish_id: UUID, dish: DishIn):
-        return self.database_repository.update_dish(
+        result = self.database_repository.update_dish(
             submenu_id=submenu_id, dish_id=dish_id, dish=dish)
+        self.cache.save_cache(subject=result, dish_id=dish_id)
+        return result
 
     def delete(self, dish_id: UUID):
+        self.cache.delete_cache(dish_id=dish_id)
         return self.database_repository.delete_dish(dish_id=dish_id)
--- menu_app/services/menu_service.py
+++ menu_app/services/menu_service.py
@@ -1,25 +1,60 @@
-from ..schemas import MenuIn
+import json
+import redis
+from typing import Optional
+from ..schemas import MenuIn, MenuOut
 from uuid import UUID
 from fastapi import Depends
 from ..repositories.menu_repository import MenuRepository
+from fastapi.encoders import jsonable_encoder
+
+
+class CacheMenu:
+    def __init__(self, host='localhost', port=6379, db=0):
+        self.redis_conn = redis.Redis(host=host, port=port, db=db)
+
+    def save_cache(self, subject: MenuOut, menu_id: UUID) -> None:
+        self.redis_conn.set(f'menu:{menu_id}', json.dumps(
+            jsonable_encoder(subject)))
+
+    def load_cache(self, menu_id: UUID) -> Optional[MenuOut]:
+        cached_data = self.redis_conn.get(f'menu:{menu_id}')
+        if cached_data:
+            return MenuOut(**json.loads(cached_data))
+        return None
+
+    def check_cache(self, menu_id: UUID) -> bool:
+        return self.redis_conn.exists(f'menu:{menu_id}')
+
+    def delete_cache(self, menu_id: UUID) -> int:
+        return self.redis_conn.delete(f'menu:{menu_id}')
 
 
 class MenuService:
-
     def __init__(self, database_repository: MenuRepository = Depends()):
         self.database_repository = database_repository
+        self.cache = CacheMenu()
 
     def get_all(self):
         return self.database_repository.get_menus()
 
     def get_one(self, menu_id: UUID):
-        return self.database_repository.get_menu(menu_id=menu_id)
+        if self.cache.check_cache(menu_id=menu_id):
+            return self.cache.load_cache(menu_id=menu_id)
+        result = self.database_repository.get_menu(menu_id=menu_id)
+        self.cache.save_cache(subject=result, menu_id=result.id)
+        return result
 
     def create(self, menu: MenuIn):
-        return self.database_repository.create_menu(menu=menu)
+        result = self.database_repository.create_menu(menu=menu)
+        self.cache.save_cache(subject=result, menu_id=result.id)
+        return result
 
     def update(self, menu: MenuIn, menu_id: UUID):
-        return self.database_repository.update_menu(menu=menu, menu_id=menu_id)
+        result = self.database_repository.update_menu(
+            menu=menu, menu_id=menu_id)
+        self.cache.save_cache(subject=result, menu_id=result.id)
+        return result
 
     def delete(self, menu_id: UUID):
+        self.cache.delete_cache(menu_id=menu_id)
         return self.database_repository.delete_menu(menu_id=menu_id)
